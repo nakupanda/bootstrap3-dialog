bc187acd6007f6d94f9a9e8be42d83f96b1f49e0
Chris Olszewski <penguin328@gmail.com>
2014-04-03 17:21:31 -0400

    Only append footer if buttons exist.

--- js/bootstrap-dialog.js
+++ js/bootstrap-dialog.js
@@ -111,7 +111,7 @@
             this.getModalContent()
             .append(this.getModalHeader())
             .append(this.getModalBody())
-            .append(this.getModalFooter());
+            .append(this.getButtons().length ? this.getModalFooter() : null);
 
             return this;
         },
